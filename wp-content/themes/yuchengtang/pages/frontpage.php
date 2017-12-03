b54949c48a5501dd55691a15896d960525678b7b
Yaowen Zhang <yaowen.zhang@live.com>
2017-12-04 06:47:25 +0800

    update index

--- wp-content/themes/yuchengtang/pages/frontpage.php
+++ wp-content/themes/yuchengtang/pages/frontpage.php
@@ -34,7 +34,7 @@
 			<p style="margin-top:5px;font-size:14px">（周日、周一闭馆 ）</p>
 			<p style="color:#3d5079;font-size:18px;margin-top:20px;border-top:1px dotted #999;padding-top:10px">上海市浦三路21弄55-56号银亿滨江中心17楼</p>
 			<div class="more">
-				<a class="btn" style="background:#3d5079">预约</a>
+				<a class="btn" style="background:#3d5079" href="<?php echo site_url('tickets');?>">预约</a>
 			</div>
 		</div>
 		<div class="antiques">
@@ -43,45 +43,45 @@
 					<img src="<?php echo get_template_directory_uri()?>/assets/images/title-antiques.png" width="60%" />
 				</div>
 				<div class="more" style="text-align:center;margin-top:80px">
-					<a class="btn" style="background:#8E5025;font-size:16px">更多</a>
+					<a class="btn" style="background:#8E5025;font-size:16px" href="<?php echo site_url('archives/category/antiques');?>">更多</a>
 				</div>
 			</div>
 			<div class="examples">
 				<ul class="accordion">
 					<li class="item1">
 						<div class="name">
-							<img src="<?php echo get_template_directory_uri()?>/assets/images/examples/example-1-name.png" width="65" />
+							<a href="<?php echo site_url('antique?id=146')?>" title="清乾隆珐琅彩九龙盘"><img src="<?php echo get_template_directory_uri()?>/assets/images/examples/example-1-name.png" width="65" /></a>
 						</div>
 						<div class="mask"></div>
 						<div class="pic">
-							<img src="<?php echo get_template_directory_uri()?>/assets/images/examples/example-1.jpg" width="100%"/>
+							<a href="<?php echo site_url('antique?id=146')?>" title="清乾隆珐琅彩九龙盘"><img src="<?php echo get_template_directory_uri()?>/assets/images/examples/example-1.jpg" width="100%"/></a>
 						</div>
 					</li>
 					<li class="item2">
 						<div class="name">
-							<img src="<?php echo get_template_directory_uri()?>/assets/images/examples/example-2-name.png" width="65" />
+							<a href="<?php echo site_url('antique?id=158')?>" title="明宣德青花缠枝纹梅瓶"><img src="<?php echo get_template_directory_uri()?>/assets/images/examples/example-2-name.png" width="65" /></a>
 						</div>
 						<div class="mask"></div>
 						<div class="pic">
-							<img src="<?php echo get_template_directory_uri()?>/assets/images/examples/example-2.jpg" width="100%" />
+							<a href="<?php echo site_url('antique?id=158')?>" title="明宣德青花缠枝纹梅瓶"><img src="<?php echo get_template_directory_uri()?>/assets/images/examples/example-2.jpg" width="100%" /></a>
 						</div>
 					</li>
 					<li class="item3">
 						<div class="name">
-							<img src="<?php echo get_template_directory_uri()?>/assets/images/examples/example-3-name.png" width="65" />
+							<a href="<?php echo site_url('antique?id=148')?>" title="元龙泉窑荷叶盖罐"><img src="<?php echo get_template_directory_uri()?>/assets/images/examples/example-3-name.png" width="65" /></a>
 						</div>
 						<div class="mask"></div>
 						<div class="pic">
-							<img src="<?php echo get_template_directory_uri()?>/assets/images/examples/example-3.jpg" width="100%" />
+							<a href="<?php echo site_url('antique?id=148')?>" title="元龙泉窑荷叶盖罐"><img src="<?php echo get_template_directory_uri()?>/assets/images/examples/example-3.jpg" width="100%" /></a>
 						</div>
 					</li>
 					<li class="item4">
 						<div class="name">
-							<img src="<?php echo get_template_directory_uri()?>/assets/images/examples/example-4-name.png" width="65" />
+							<a href="<?php echo site_url('antique?id=150')?>" title="宋汝窑鼓钉洗"><img src="<?php echo get_template_directory_uri()?>/assets/images/examples/example-4-name.png" width="65" /></a>
 						</div>
 						<div class="mask"></div>
 						<div class="pic">
-							<img src="<?php echo get_template_directory_uri()?>/assets/images/examples/example-4.jpg" width="100%" />
+							<a href="<?php echo site_url('antique?id=150')?>" title="宋汝窑鼓钉洗"><img src="<?php echo get_template_directory_uri()?>/assets/images/examples/example-4.jpg" width="100%" /></a>
 						</div>
 					</li>
 					
@@ -99,7 +99,6 @@ jQuery(function($) {
 		slideWidth: 330,
 		//tabWidth: "15%",
 		width: 650,
-		
 		slideClass: "slider",
 		height: 500,
 		trigger: "mouseover",
