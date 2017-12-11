5049a8141473c255e22035b42e92f2c67603ee0f
Yaowen Zhang <yaowen.zhang@live.com>
2017-12-12 01:22:55 +0800

    update cates

--- wp-content/themes/yuchengtang/category-antiques.php
+++ wp-content/themes/yuchengtang/category-antiques.php
@@ -1,8 +1,7 @@
 <?php
-if (!is_mobile()) {
-	wp_enqueue_script( 'pagepiling',  get_template_directory_uri() . '/assets/js/pagepiling/jquery.pagepiling.min.js', ['jquery']);
-	wp_enqueue_style( 'pagepiling-style', get_template_directory_uri() . '/assets/js/pagepiling/jquery.pagepiling.min.css' );
-}
+wp_enqueue_script( 'pagepiling',  get_template_directory_uri() . '/assets/js/pagepiling/jquery.pagepiling.min.js', ['jquery']);
+wp_enqueue_style( 'pagepiling-style', get_template_directory_uri() . '/assets/js/pagepiling/jquery.pagepiling.min.css' );
+
 
 /** 获取各个分类的藏品 **/
 global $wpdb;
@@ -26,30 +25,35 @@ $categories = [
 <?php get_header(); ?>
 <div id="pagepiling" style="height:1000px">
 	<div class="section categories">
-		<table>
-			<tr>
-				<td>
-					<div class="mask"></div>
-					<a href="#dyn-qing"><img src="<?php echo get_template_directory_uri()?>/assets/images/category-dyn-qing.png" /></a>
-				</td>
-				<td>
-					<div class="mask"></div>
-					<a href="#dyn-ming"><img src="<?php echo get_template_directory_uri()?>/assets/images/category-dyn-ming.png" /></a></td>
-				<td><div class="mask"></div><a href="#dyn-yuan"><img src="<?php echo get_template_directory_uri()?>/assets/images/category-dyn-yuan.png" /></a></td>
-				<td><div class="mask"></div><a href="#dyn-song"><img src="<?php echo get_template_directory_uri()?>/assets/images/category-dyn-song.png" /></a></td>
-			</tr>
-			<tr>
-				<td><div class="mask"></div><a href="#exb-g20"><img src="<?php echo get_template_directory_uri()?>/assets/images/category-exb-g20.png" /></a></td>
-				<td><div class="mask"></div><a href="#exb-b5"><img src="<?php echo get_template_directory_uri()?>/assets/images/category-exb-b5.png" /></a></td>
-				<td></td>
-				<td></td>
-			</tr>
-		</table>
+		<div class="cate-node">
+			<div class="mask"></div>
+			<a href="#dyn-qing"><img src="<?php echo get_template_directory_uri()?>/assets/images/category-dyn-qing.png" /></a>
+		</div>
+		<div class="cate-node">
+			<div class="mask"></div>
+			<a href="#dyn-ming"><img src="<?php echo get_template_directory_uri()?>/assets/images/category-dyn-ming.png" /></a>
+		</div>
+		<div class="cate-node">
+			<div class="mask"></div><a href="#dyn-yuan"><img src="<?php echo get_template_directory_uri()?>/assets/images/category-dyn-yuan.png" /></a>
+		</div>
+		<div class="cate-node">
+			<div class="mask"></div><a href="#dyn-song"><img src="<?php echo get_template_directory_uri()?>/assets/images/category-dyn-song.png" />
+		</div>
+		<div class="cate-node">
+			<div class="mask"></div><a href="#exb-g20"><img src="<?php echo get_template_directory_uri()?>/assets/images/category-exb-g20.png" /></a></td>
+		</div>
+		<div class="cate-node">
+			<div class="mask"></div><a href="#exb-b5"><img src="<?php echo get_template_directory_uri()?>/assets/images/category-exb-b5.png" /></a></td>
+		</div>
+		<div class="clear"></div>
 	</div>
 	<?php foreach ($categories as $k => $v):?>
 	<div class="section">
 		<div class="wrap sly">
 			<h2><?php echo $v[0]?>&nbsp;<small><?php echo $v[1]?></small></h2>
+			<div class="mobile" style="width:30px;margin:auto;padding-top:5px;margin-bottom:5px">
+				<img src="<?php echo get_template_directory_uri()?>/assets/images/dot-divider.png" width="100%">
+			</div>
 			<div class="scrollbar">
 				<div class="handle">
 					<div class="mousearea"></div>
@@ -79,6 +83,11 @@ $categories = [
 		</div>
 	</div>
 	<?php endforeach;?>
+</div>
+<div id="mobile-guide" class="mobile hidden">
+	<a class="prev-slide">&lt;&nbsp;上页</a>&nbsp;|&nbsp;
+	<a class="home-slide">所有分类</a>&nbsp;|&nbsp;
+	<a class="next-slide">下页&nbsp;&gt;</a>
 </div>
 <script>
 jQuery(function($) {
@@ -126,19 +135,21 @@ jQuery(function($) {
 	$('#pagepiling').pagepiling({
 	    menu: null,
         direction: 'vertical',
-        verticalCentered: true,
+        verticalCentered: <?php echo is_mobile() ? 'false' : 'true' ?>,
         sectionsColor: [],
         anchors: ['all-cates', 'dyn-qing', 'dyn-ming', 'dyn-yuan', 'dyn-song', 'exb-g20', 'exb-b5'],
-        scrollingSpeed: 700,
+        scrollingSpeed: 500,
         easing: 'swing',
-        loopBottom: false,
+        loopBottom: true,
         loopTop: false,
         css3: true,
         navigation: {
             'textColor': '#000',
             'bulletsColor': '#666',
             'position': 'right',
+            <?php if (!is_mobile()):?>
             'tooltips': ['藏品分类 All Categories', '清代 Qing Dynasty', '明代 Ming Dynasty', '元代 Yuan Dynasty', '宋代 Song Dynasty', 'G20会议官窑瓷器', '金砖五国会议官窑瓷器']
+            <?php endif; ?>
         },
        	normalScrollElements: null,
         normalScrollElementTouchThreshold: 5,
@@ -148,6 +159,11 @@ jQuery(function($) {
         animateAnchor: false,
 		//events
 		onLeave: function(index, nextIndex, direction) {
+			if (nextIndex == 1) {
+				$('#mobile-guide').addClass('hidden');
+			} else {
+				$('#mobile-guide').removeClass('hidden');
+			}
 			$('#pagepiling .section').css('visibility', 'hidden');
 			$('#pagepiling .section.active').css('visibility', 'visible');
 		},
@@ -158,6 +174,17 @@ jQuery(function($) {
 			$('#pagepiling .section.active').css('visibility', 'visible');
 		},
 	});
+
+	$('#mobile-guide .prev-slide').click(function() {
+		$('#pagepiling').pagepiling.moveSectionUp();
+	});
+	$('#mobile-guide .next-slide').click(function() {
+		$('#pagepiling').pagepiling.moveSectionDown();
+	});
+	$('#mobile-guide .home-slide').click(function() {
+		$('#pagepiling').pagepiling.moveTo(1);
+	});
+	
 });
 </script>
 <?php
