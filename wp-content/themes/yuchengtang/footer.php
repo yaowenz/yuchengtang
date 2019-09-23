720948820790609790d005616fefbf6e59c2c491
Willis Zhang <yaowen.zhang@qq.com>
2019-09-23 14:41:33 +0800

    update footer

--- wp-content/themes/yuchengtang/footer.php
+++ wp-content/themes/yuchengtang/footer.php
@@ -1,9 +1,9 @@
 <div id="footer">
 	<div class="wrapper desktop">
-		Copyright 2017 上海御承文化发展有限公司  | 沪ICP备15055452号-1
+		Copyright 2017 上海御承文化发展有限公司  | <a href="http://www.miitbeian.gov.cn">沪ICP备15055452号-1</a>
 	</div>
 	<div class="wrapper mobile">
-		上海御承文化发展有限公司，沪ICP备15055452号-1
+		上海御承文化发展有限公司，<a href="http://www.miitbeian.gov.cn">沪ICP备15055452号-1</a>
 	</div>
 </div>
 <?php wp_footer(); ?>
